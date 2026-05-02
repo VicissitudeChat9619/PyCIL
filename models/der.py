c1055a2fadfaf7e06ece583957be75a894f5e831
VicissitudeChat9619 <2223028651@qq.com>
2026-05-03 04:38:07 +0800

    发现der复习的问题并修改

--- models/der.py
+++ models/der.py
@@ -66,13 +66,13 @@ class DER(BaseLearner):
             appendent=self._get_memory(),
         )
         self.train_loader = DataLoader(
-            train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=self._num_workers
         )
         test_dataset = data_manager.get_dataset(
             np.arange(0, self._total_classes), source="test", mode="test"
         )
         self.test_loader = DataLoader(
-            test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
+            test_dataset, batch_size=batch_size, shuffle=False, num_workers=self._num_workers
         )
 
         if len(self._multiple_gpus) > 1:
@@ -107,16 +107,29 @@ class DER(BaseLearner):
             )
             self._init_train(train_loader, test_loader, optimizer, scheduler)
         else:
-            optimizer = optim.SGD(
+            # 阶段1优化器：新骨干 + fc
+            optimizer_stage1 = optim.SGD(
                 filter(lambda p: p.requires_grad, self._network.parameters()),
                 lr=lrate,
                 momentum=0.9,
                 weight_decay=weight_decay,
             )
-            scheduler = optim.lr_scheduler.MultiStepLR(
-                optimizer=optimizer, milestones=milestones, gamma=lrate_decay
+            scheduler_stage1 = optim.lr_scheduler.MultiStepLR(
+                optimizer=optimizer_stage1, milestones=milestones, gamma=lrate_decay
             )
-            self._update_representation(train_loader, test_loader, optimizer, scheduler)
+            # 阶段2优化器：只包含 fc
+            optimizer_stage2 = optim.SGD(
+                filter(lambda p: p.requires_grad, self._network.fc.parameters()),
+                lr=lrate,
+                momentum=0.9,
+                weight_decay=weight_decay,
+            )
+            scheduler_stage2 = optim.lr_scheduler.MultiStepLR(
+                optimizer=optimizer_stage2, milestones=milestones, gamma=lrate_decay
+            )
+            self._update_representation(train_loader, test_loader, 
+                                        optimizer_stage1, scheduler_stage1,
+                                        optimizer_stage2, scheduler_stage2)
             if len(self._multiple_gpus) > 1:
                 self._network.module.weight_align(
                     self._total_classes - self._known_classes
@@ -134,7 +147,7 @@ class DER(BaseLearner):
                 inputs, targets = inputs.to(self._device), targets.to(self._device)
                 logits = self._network(inputs)["logits"]
 
-                loss = F.cross_entropy(logits, targets)
+                loss = F.cross_entropy(logits, targets.long())
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
@@ -169,62 +182,128 @@ class DER(BaseLearner):
 
         logging.info(info)
 
-    def _update_representation(self, train_loader, test_loader, optimizer, scheduler):
+    def _update_representation(self, train_loader, test_loader, 
+                                optimizer_stage1, scheduler_stage1,
+                                optimizer_stage2, scheduler_stage2):
+        # ========== 阶段1：训练新骨干 + fc + aux_fc ==========
+        logging.info("=" * 50)
+        logging.info("Stage 1: Training convnet + fc + aux_fc")
+        logging.info("=" * 50)
+        
         prog_bar = tqdm(range(epochs))
         for _, epoch in enumerate(prog_bar):
             self.train()
             losses = 0.0
-            losses_clf = 0.0
-            losses_aux = 0.0
             correct, total = 0, 0
-            for i, (_, inputs, targets) in enumerate(train_loader):
+            
+            for _, (_, inputs, targets) in enumerate(train_loader):
                 inputs, targets = inputs.to(self._device), targets.to(self._device)
                 outputs = self._network(inputs)
-                logits, aux_logits = outputs["logits"], outputs["aux_logits"]
-                loss_clf = F.cross_entropy(logits, targets)
-                aux_targets = targets.clone()
-                aux_targets = torch.where(
-                    aux_targets - self._known_classes + 1 > 0,
-                    aux_targets - self._known_classes + 1,
-                    0,
-                )
-                loss_aux = F.cross_entropy(aux_logits, aux_targets)
-                loss = loss_clf + loss_aux
-
-                optimizer.zero_grad()
+                logits = outputs["logits"]
+                
+                loss = F.cross_entropy(logits, targets.long())
+                
+                optimizer_stage1.zero_grad()
                 loss.backward()
-                optimizer.step()
+                optimizer_stage1.step()
+                
                 losses += loss.item()
-                losses_aux += loss_aux.item()
-                losses_clf += loss_clf.item()
-
                 _, preds = torch.max(logits, dim=1)
                 correct += preds.eq(targets.expand_as(preds)).cpu().sum()
                 total += len(targets)
 
-            scheduler.step()
+            scheduler_stage1.step()
             train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+            
             if epoch % 5 == 0:
                 test_acc = self._compute_accuracy(self._network, test_loader)
-                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Loss_clf {:.3f}, Loss_aux {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
+                info = "Task {}, Stage1 Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
                     self._cur_task,
                     epoch + 1,
                     epochs,
                     losses / len(train_loader),
-                    losses_clf / len(train_loader),
-                    losses_aux / len(train_loader),
                     train_acc,
                     test_acc,
                 )
             else:
-                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Loss_clf {:.3f}, Loss_aux {:.3f}, Train_accy {:.2f}".format(
+                info = "Task {}, Stage1 Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}".format(
                     self._cur_task,
                     epoch + 1,
                     epochs,
                     losses / len(train_loader),
-                    losses_clf / len(train_loader),
-                    losses_aux / len(train_loader),
                     train_acc,
                 )
             prog_bar.set_description(info)
         logging.info(info)
+
+        # ========== 阶段2：重新初始化 fc，只训练 fc ==========
+        logging.info("=" * 50)
+        logging.info("Stage 2: Reinitializing fc, training fc only")
+        logging.info("=" * 50)
+        
+        # 重新初始化 fc
+        self._network.fc = self._network.generate_fc(
+            self._network.feature_dim, self._total_classes
+        ).to(self._device)
+        
+        # 重新创建阶段2优化器（只包含新的 fc）
+        optimizer_stage2.param_groups = []
+        optimizer_stage2.add_param_group(
+            {'params': filter(lambda p: p.requires_grad, self._network.fc.parameters())}
+        )
+        # 重置 scheduler 的 milestones
+        scheduler_stage2 = optim.lr_scheduler.MultiStepLR(
+            optimizer=optimizer_stage2, milestones=milestones, gamma=lrate_decay
+        )
+        
+        prog_bar = tqdm(range(epochs))
+        for _, epoch in enumerate(prog_bar):
+            self.train()
+            losses = 0.0
+            correct, total = 0, 0
+            
+            for _, (_, inputs, targets) in enumerate(train_loader):
+                inputs, targets = inputs.to(self._device), targets.to(self._device)
+                outputs = self._network(inputs)
+                logits = outputs["logits"]
+                
+                loss = F.cross_entropy(logits, targets.long())
+                
+                optimizer_stage2.zero_grad()
+                loss.backward()
+                optimizer_stage2.step()
+                
+                losses += loss.item()
+                _, preds = torch.max(logits, dim=1)
+                correct += preds.eq(targets.expand_as(preds)).cpu().sum()
+                total += len(targets)
+
+            scheduler_stage2.step()
+            train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+            
+            if epoch % 5 == 0:
+                test_acc = self._compute_accuracy(self._network, test_loader)
+                info = "Task {}, Stage2 Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    epochs,
+                    losses / len(train_loader),
+                    train_acc,
+                    test_acc,
+                )
+            else:
+                info = "Task {}, Stage2 Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    epochs,
+                    losses / len(train_loader),
+                    train_acc,
+                )
+            prog_bar.set_description(info)
+        logging.info(info)
+
+
+
+class DER_A(DER):
+    def __init__(self, args):
+        super().__init__(args)
